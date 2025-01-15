56c4e6a5c2a39bab473ee75c9092844a5ec95ff6
rfccg <rafael.gouveia@incognia.com>
2025-01-15 10:09:20 -0300

    add two sum complete in go

--- go/two_sum.go
+++ go/two_sum.go
@@ -2,11 +2,47 @@ package main
 
 import (
 	"fmt"
-	// "sort"
+	"sort"
 )
 
+type IdxSlice struct {
+	sort.IntSlice
+	idx []int
+}
+
+func makeRange(min, max int) []int {
+	a := make([]int, max-min+1)
+	for i := range a {
+		a[i] = min + i
+	}
+	return a
+}
+
+func (s IdxSlice) Swap(i, j int) {
+	s.IntSlice.Swap(i, j)
+	s.idx[i], s.idx[j] = s.idx[j], s.idx[i]
+}
+
 func twoSum(nums []int, target int) []int {
-	return []int{0, 1}
+	idxArr := makeRange(0, len(nums))
+	idxPreserve := IdxSlice{nums, idxArr}
+	sort.Sort(idxPreserve)
+
+	size := len(nums)
+	i := 0
+	j := size - 1
+
+	for i < j {
+		sum := idxPreserve.IntSlice[i] + idxPreserve.IntSlice[j]
+		if sum == target {
+			return []int{idxPreserve.idx[i], idxPreserve.idx[j]}
+		} else if sum < target {
+			i++
+		} else {
+			j--
+		}
+	}
+	return []int{idxPreserve.idx[i], idxPreserve.idx[j]}
 }
 
 func main() {
