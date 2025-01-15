f012e2717890b751c5640105e66ca2dddc4577bc
rfccg <rafael.gouveia@incognia.com>
2025-01-15 09:19:25 -0300

    add file

---
+++ go/two_sum.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	// "sort"
+)
+
+func twoSum(nums []int, target int) []int {
+	return []int{0, 1}
+}
+
+func main() {
+	numbers := []int{1, 2, 54, 7, 2, 3, 7, 8}
+	twoSumR := twoSum(numbers, 15)
+
+	fmt.Println(twoSumR)
+}
