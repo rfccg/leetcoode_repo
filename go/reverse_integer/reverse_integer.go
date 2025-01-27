146bebe5319dd4f534ba6d6a8cad564451309f33
rfccg <rafael.gouveia@incognia.com>
2025-01-27 14:40:13 -0300

    reverse integer with better overflow detection

--- go/reverse_integer/reverse_integer.go
+++ go/reverse_integer/reverse_integer.go
@@ -2,40 +2,45 @@ package main
 
 import (
 	"fmt"
-	"math"
+	// "math"
 )
 
-func reverse(x int) int {
-	acc := 0
-	multiplier := 10
-	overflowDetection := x > 0
+func reverse(x int32) int32 {
+	acc := int32(0)
+	multiplier := int32(10)
+	overflowDetection := acc
 	for x != 0 {
+		overflowDetection = acc
 		acc *= multiplier
 		digit := x % 10
 		acc += digit
 		x /= 10
-		if (acc > 0) != overflowDetection && acc != 0 {
+		fmt.Printf("acc %d, ofd %d\n", acc, overflowDetection)
+		if acc%10 != digit {
 			// fmt.Println("Hit overflowDetection")
 			// fmt.Printf("%d, %d \n", x, acc)
 			return 0
 		}
 	}
-	maxValue := int(math.Pow(2, 31))
+	// maxValue := int32(math.Pow(2, 31))
 	// fmt.Printf("%d\n", maxValue)
-	if acc > maxValue-1 || acc < -maxValue {
-		return 0
-	}
+	// if acc > maxValue-1 || acc < -maxValue {
+	// 	return 0
+	// }
 	return acc
 }
 
 func main() {
-	fmt.Println(reverse(10))
-	fmt.Println(reverse(11))
+	a := int32(10)
 
+	// fmt.Println(a * math.MaxInt32)
+	// fmt.Println(reverse(10))
+	// fmt.Println(reverse(11))
+	fmt.Println(int32(964632435) * a)
 	fmt.Println(reverse(1534236469))
-	fmt.Println(reverse(210))
-	fmt.Println(reverse(101))
-	fmt.Println(reverse(-1315))
-	fmt.Println(reverse(1))
-	fmt.Println(reverse(0))
+	// fmt.Println(reverse(210))
+	// fmt.Println(reverse(101))
+	// fmt.Println(reverse(-1315))
+	// fmt.Println(reverse(1))
+	// fmt.Println(reverse(0))
 }
